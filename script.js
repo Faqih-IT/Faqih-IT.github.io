8e46385163f9a6162c7b4e390cd1499dbb1cdf97
Faqih-IT <discord310309@gmail.com>
2024-08-22 00:39:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// The script for the icon font from https://akaricons.com/ is in this pen's JS settings
+
+function goUrl() {
+    window.location.href = './assets/404/index.html';
+}
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
